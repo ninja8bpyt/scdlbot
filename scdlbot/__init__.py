0ce18b04b610d672c1b6597a37a6211847f2f1b1
ninja8bpyt <78526646+ninja8bpyt@users.noreply.github.com>
2021-03-01 09:54:33 +0530

    Update __init__.py

--- scdlbot/__init__.py
+++ scdlbot/__init__.py
@@ -2,6 +2,6 @@
 
 """Top-level package for Music Downloader Telegram Bot."""
 
-__author__ = """George Pchelkin"""
-__email__ = "george@pchelk.in"
+__author__ = """@Bots_Ki_Duniya"""
+__reportbugs__ = "@Mr_Ninjas_Bot"
 __version__ = "0.13.7"
